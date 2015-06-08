57a318e7332868f78e7d5c29b314e9e1ad1984c3
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-06-08 07:46:10 -0400

    0.3

--- modal.js
+++ modal.js
@@ -1,6 +1,14 @@
+//Modal.js is by Cosmic Open Source Projects v0.2
+
+//How it works?
+
+//1. Saves your old code!
+//2. Adds a script to the end and adds your prefrences to it!
+//3. Redirects to it!
+
 modaljs = function(title,body) {
 
-var was = document.body.innerHTML
+  var was = document.body.innerHTML;
 
   document.body.innerHTML = was + "<div id='openModal' class='modal'><div><a href='#close' title='Close' class='close'>X</a><h2>" + title + "</h2><p>" + body + "</p></div></div>";
   window.location = "#openModal";
