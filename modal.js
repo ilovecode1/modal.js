6b57fa68c6d8ca11dbb486cf6e33050d1d204422
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-08-29 20:19:15 -0400

    Update modal.js

--- modal.js
+++ modal.js
@@ -10,7 +10,7 @@ modaljs = function(title,body) {
 
   var was = document.body.innerHTML;
 
-  document.body.innerHTML = was + "<div id='openModal' class='modal'><div><a href='#close' title='Close' class='close'>X</a><h2>" + title + "</h2><p>" + body + "</p></div></div>";
+  document.body.innerHTML = was + "<div id='openModal' class='modal'><div><a href='#close' title='Close' class='close'>X</a><h2 class='title'>" + title + "</h2><p class='body'>" + body + "</p></div></div>";
   window.location = "#openModal";
 
 }
